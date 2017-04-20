934af8b9b06705f64913c1e12feaf3990a029bc9
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-04-19 18:20:07 -0700

    update

---
+++ oo/class.ts
@@ -0,0 +1,16 @@
+// Use constructor parameters as fields.
+class Person2{
+   constructor(
+      public firstName: string,
+      public lastName: string,
+      public email: string,
+      private age: number
+   ) { }
+
+   greetMe() {
+      console.log(`Hello ${this.firstName}`);
+   }
+}
+
+let p2 = new Person2('Jesse', 'Liberty', 'examplename@gmail.com', 21);
+p2.greetMe();
